48d912e8aa299a9c04b09806a7012aedf1a09482
Cheth <cheth@Cheths-MacBook-Pro.local>
2019-01-26 14:59:49 +1100

    TODO: few final commits

--- Banking_Application/Program.cs
+++ Banking_Application/Program.cs
@@ -6,7 +6,8 @@ namespace Banking_Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankingSystem bankingSystem = new BankingSystem();
+            bankingSystem.Start();
         }
     }
 }
